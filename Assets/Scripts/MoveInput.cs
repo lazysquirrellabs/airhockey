36fe0b6b1393773e4660af54ed796ff2a28296fd
Matheus Andrade <matheus.amazonas@gmail.com>
2020-05-30 13:23:21 +0200

    :sparkles: Add basic collision velocity transfer

--- Assets/Scripts/MoveInput.cs
+++ Assets/Scripts/MoveInput.cs
@@ -1,3 +1,23 @@
-$HEADER$namespace $NAMESPACE$ {
-  public class $CLASS$ : UnityEngine.MonoBehaviour {$END$}
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class MoveInput : MonoBehaviour
+    {
+        #region Serialized fields
+
+        [SerializeField] private Rigidbody2D _rigidbody;
+
+        #endregion
+
+        #region Update
+
+        private void Update()
+        {
+            if (Input.GetKeyUp(KeyCode.D))
+                _rigidbody.AddForce(new Vector2(15,0));
+        }
+
+        #endregion
+    }
 }
--- Assets/Scripts/Physics/VelocityTransfer.cs
+++ Assets/Scripts/Physics/VelocityTransfer.cs
@@ -1,7 +1,23 @@
-namespace DefaultNamespace
+using UnityEngine;
+
+namespace AirHockey.Physics
 {
-    public class VelocityTransfer
+    public class VelocityTransfer : MonoBehaviour
     {
+        #region Serialized fields
+
+        [SerializeField] private Rigidbody2D _rigidbody;
+
+        #endregion
         
+        #region Event handlers
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            Debug.Log($"Collised with velocity {other.relativeVelocity}");
+            _rigidbody.velocity = other.relativeVelocity;
+        }
+
+        #endregion
     }
 }
