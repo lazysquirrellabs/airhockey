5e30d2f4457b27e0b4e58ea1b429989bb35c8e6c
Matheus Amazonas <matheus.amazonas@gmail.com>
2026-07-26 12:13:17 +0200

    🌐 Localize Play screen
    The only thing missing there is the popup, which will be localized later.

--- Assets/Scripts/Match/MatchMode.cs
+++ Assets/Scripts/Match/MatchMode.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace LazySquirrelLabs.AirHockey.Match
 {
 	/// <summary>
@@ -27,64 +25,5 @@ namespace LazySquirrelLabs.AirHockey.Match
 		/// The match never ends unless the players leave it.
 		/// </summary>
 		Endless
-	}
-
-	internal static class MatchModeExtensions
-	{
-		#region Internal
-
-		/// <summary>
-		/// Fetches the name of the extra information needed by a <paramref name="matchMode"/> to be valid.
-		/// </summary>
-		/// <param name="matchMode">The mode.</param>
-		/// <returns>A friendly name for the extra information.</returns>
-		/// <exception cref="ArgumentOutOfRangeException">Thrown whenever the given <paramref name="matchMode"/>
-		/// does not require extra information.</exception>
-		/// <exception cref="NotImplementedException">Thrown whenever the given <paramref name="matchMode"/> has not
-		/// been implemented yet.</exception>
-		internal static string InfoName(this MatchMode matchMode)
-		{
-			switch (matchMode)
-			{
-				case MatchMode.HighScore:
-				case MatchMode.BestOfScore:
-					return "score";
-				case MatchMode.Time:
-					return "duration";
-				case MatchMode.Endless:
-					const string message = "Endless mode doesn't require info.";
-					throw new ArgumentOutOfRangeException(nameof(matchMode), matchMode, message);
-				default:
-					throw new NotImplementedException($"Mode not implemented: {matchMode}.");
-			}
-		}
-
-		/// <summary>
-		/// Fetches the units of the extra information needed by a <paramref name="matchMode"/> to be valid.
-		/// </summary>
-		/// <param name="matchMode">The mode.</param>
-		/// <returns>A friendly name for the extra information's unit.</returns>
-		/// <exception cref="ArgumentOutOfRangeException">Thrown whenever the given <paramref name="matchMode"/>
-		/// does not require extra information.</exception>
-		/// <exception cref="NotImplementedException">Thrown whenever the given <paramref name="matchMode"/> has not
-		/// been implemented yet.</exception>
-		internal static string InfoUnitName(this MatchMode matchMode)
-		{
-			switch (matchMode)
-			{
-				case MatchMode.HighScore:
-				case MatchMode.BestOfScore:
-					return "points";
-				case MatchMode.Time:
-					return "minutes";
-				case MatchMode.Endless:
-					const string message = "Endless mode doesn't require info.";
-					throw new ArgumentOutOfRangeException(nameof(matchMode), matchMode, message);
-				default:
-					throw new NotImplementedException($"Mode not implemented: {matchMode}.");
-			}
-		}
-
-		#endregion
 	}
 }
--- Assets/Scripts/Menu/PlayScreen.cs
+++ Assets/Scripts/Menu/PlayScreen.cs
@@ -4,6 +4,8 @@ using LazySquirrelLabs.AirHockey.Match;
 using LazySquirrelLabs.AirHockey.UI.Menu;
 using LazySquirrelLabs.AirHockey.UI.Popups;
 using UnityEngine;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Components;
 using UnityEngine.UI;
 using Screen = LazySquirrelLabs.AirHockey.UI.Screen;
 
@@ -28,19 +30,26 @@ namespace LazySquirrelLabs.AirHockey.Menu
 		[SerializeField] private ModeToggleGroup _modeSelector;
 		[SerializeField] private Button _startButton;
 		[SerializeField] private InputField _extraInfoInput;
-		[SerializeField] private Text _extraFieldLabel;
-		[SerializeField] private Text _extraInfoUnit;
 		[SerializeField] private MessagePopup _popup;
 		[SerializeField, TextArea] private string _endlessModeWarning;
+		[Header("Localization")]
+		[SerializeField] private LocalizeStringEvent _extraInfoLocalization;
+		[SerializeField] private LocalizeStringEvent _unitLocalization;
+		[SerializeField] private LocalizedString _scoreLocalizationKey;
+		[SerializeField] private LocalizedString _durationLocalizationKey;
+		[SerializeField] private LocalizedString _pointsLocalizationKey;
+		[SerializeField] private LocalizedString _minutesLocalizationKey;
 
 		#endregion
 
 		#region Fields
 
+		private const string ExtraInfoKey = "extra_info";
 		private MatchMode _matchMode;
 		private uint _extraInfo;
 		private bool _needsExtraInfo;
 		private bool _validExtraInfo;
+		private LocalizedString _extraInfoVariable;
 
 		#endregion
 
@@ -51,6 +60,10 @@ namespace LazySquirrelLabs.AirHockey.Menu
 			base.Awake();
 			_startButton.onClick.AddListener(HandleStart);
 			_modeSelector.OnSelect += HandleModeSelect;
+			if (_extraInfoLocalization.StringReference[ExtraInfoKey] is LocalizedString variable)
+				_extraInfoVariable = variable;
+			else
+				Debug.LogWarning($"Extra info variable mismatch with key: {ExtraInfoKey}");
 		}
 
 		protected override void OnDestroy()
@@ -71,7 +84,8 @@ namespace LazySquirrelLabs.AirHockey.Menu
 		{
 			if (_needsExtraInfo && !TryGetExtraInfo())
 			{
-				_popup.Message = $"PROVIDE THE MODE {_matchMode.InfoName().ToUpper()} BEFORE STARTING A MATCH";
+				// TODO: Fix popup
+				// _popup.Message = $"PROVIDE THE MODE {_matchMode.InfoName().ToUpper()} BEFORE STARTING A MATCH";
 				_popup.Show();
 				return;
 			}
@@ -120,22 +134,22 @@ namespace LazySquirrelLabs.AirHockey.Menu
 		private void HandleModeSelect(MatchMode matchMode)
 		{
 			_matchMode = matchMode;
-
+			_extraInfoInput.text = string.Empty;
 			switch (matchMode)
 			{
 				case MatchMode.HighScore:
 				case MatchMode.BestOfScore:
 				case MatchMode.Time:
 					_extraInfoInput.gameObject.SetActive(true);
-					_extraFieldLabel.text = $"INSERT {_matchMode.InfoName().ToUpper()} HERE";
-					_extraInfoInput.text = "";
-					_extraInfoUnit.text = _matchMode.InfoUnitName().ToUpper();
+					var extraInfoLocalization = GetMatchModeExtraInfoLocalization(matchMode);
+					_extraInfoVariable.TableEntryReference = extraInfoLocalization.TableEntryReference;
+					var unitLocalization = GetMatchModeUnitLocalization(matchMode);
+					_unitLocalization.StringReference = unitLocalization;
 					_needsExtraInfo = true;
 					break;
 				case MatchMode.Endless:
 					_extraInfoInput.gameObject.SetActive(false);
 					_needsExtraInfo = false;
-					_extraInfoUnit.text = "";
 					break;
 				default:
 					throw new NotImplementedException($"Match mode not implemented: {_matchMode}");
@@ -152,6 +166,63 @@ namespace LazySquirrelLabs.AirHockey.Menu
 			gameObject.SetActive(false);
 			_popup.Hide();
 			base.Hide();
+		}
+
+		#endregion
+
+		#region Private
+
+		/// <summary>
+		/// Fetches the localization key of the extra information needed by a <paramref name="matchMode"/> to be valid.
+		/// </summary>
+		/// <param name="matchMode">The mode.</param>
+		/// <returns>The localization key for the extra information.</returns>
+		/// <exception cref="ArgumentOutOfRangeException">Thrown whenever the given <paramref name="matchMode"/>
+		/// does not require extra information.</exception>
+		/// <exception cref="NotImplementedException">Thrown whenever the given <paramref name="matchMode"/> has not
+		/// been implemented yet.</exception>
+		private LocalizedString GetMatchModeExtraInfoLocalization(MatchMode matchMode)
+		{
+			switch (matchMode)
+			{
+				case MatchMode.HighScore:
+				case MatchMode.BestOfScore:
+					return _scoreLocalizationKey;
+				case MatchMode.Time:
+					return _durationLocalizationKey;
+				case MatchMode.Endless:
+					const string message = "Endless mode doesn't require info.";
+					throw new ArgumentOutOfRangeException(nameof(matchMode), matchMode, message);
+				default:
+					throw new NotImplementedException($"Mode not implemented: {matchMode}.");
+			}
+		}
+
+		/// <summary>
+		/// Fetches the localization key of units of the extra information needed by a <paramref name="matchMode"/>
+		/// to be valid.
+		/// </summary>
+		/// <param name="matchMode">The mode.</param>
+		/// <returns>The localization key for the extra information's unit.</returns>
+		/// <exception cref="ArgumentOutOfRangeException">Thrown whenever the given <paramref name="matchMode"/>
+		/// does not require extra information.</exception>
+		/// <exception cref="NotImplementedException">Thrown whenever the given <paramref name="matchMode"/> has not
+		/// been implemented yet.</exception>
+		private LocalizedString GetMatchModeUnitLocalization(MatchMode matchMode)
+		{
+			switch (matchMode)
+			{
+				case MatchMode.HighScore:
+				case MatchMode.BestOfScore:
+					return _pointsLocalizationKey;
+				case MatchMode.Time:
+					return _minutesLocalizationKey;
+				case MatchMode.Endless:
+					const string message = "Endless mode doesn't require info.";
+					throw new ArgumentOutOfRangeException(nameof(matchMode), matchMode, message);
+				default:
+					throw new NotImplementedException($"Mode not implemented: {matchMode}.");
+			}
 		}
 
 		#endregion
